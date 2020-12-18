04503ad576138283688d938094c0e10b6b5ca636
lorenzo <lorenzotquiambao@gmail.com>
2020-12-18 15:59:07 -0500

    completed book router get requests

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -1,10 +1,37 @@
 const express = require('express');
-const bookRouter = express.Router();
+const path = require('path');
+const debug = require('debug')(`app:${path.basename(__filename, '.js')}`)
 
-bookRouter.route('/books')
-  .get((req, res) => {
-    const response = { hello: "This is my API" }
-    res.json(response);
-  });
+const router = express.Router();
+
+function bookRouter(Books) {
+  const queryHandler = (reqQuery) => {
+    const query = {};
+    if (reqQuery.title) query.title = reqQuery.title;
+    if (reqQuery.author) query.author = reqQuery.author;
+    if (reqQuery.genre) query.genre = reqQuery.genre;
+    if (reqQuery.read === 'true' || reqQuery.read === 'false') query.read = reqQuery.read;
+    return query;
+  };
+
+  router.route('/books')
+    .get((req, res) => {
+      const query = queryHandler(req.query);
+      debug(query);
+      Books.find(query)
+        .then((books) => res.json(books))
+        .catch((err) => res.send(err));
+    });
+
+  router.route('/books/:bookId')
+    .get((req, res) => {
+      const { bookId } = req.params;
+      Books.findById(bookId)
+        .then((book) => res.json(book))
+        .catch((err) => res.send(err));
+    });
+
+  return router;
+}
 
 module.exports = bookRouter;
