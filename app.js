504e82db151396ae3a7dae83654301ced080571d
lorenzo <lorenzotquiambao@gmail.com>
2020-12-18 15:57:48 -0500

    added router and connection to db

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
+// require necessary packages
 const express = require('express');
+const mongoose = require('mongoose');
 const debug = require('debug')('app');
 
-const app = express();
+// setup db connection string
+const connectionString = 'mongodb://localhost:27017/bookAPI';
+// setup port configured from package.json > nodemonConfig
 const port = process.env.PORT || 3000;
 
-const bookRouter = require('./routes/bookRouter');
+// initialize server
+const app = express();
+// initialize db connection
+mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const BookModel = require('./models/bookModel');
+const bookRouter = require('./routes/bookRouter')(BookModel);
+
+// router
 app.use('/api', bookRouter);
 
 app.get('/', (req, res) => {
