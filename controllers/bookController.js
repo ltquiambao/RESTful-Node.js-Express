39a688d2ea2639f6aac8b344116071999bf11b4a
lorenzo <lorenzotquiambao@gmail.com>
2020-12-18 22:24:50 -0500

    transferred logic from router to controller module

---
+++ controllers/bookController.js
@@ -0,0 +1,87 @@
+const path = require('path');
+const debug = require('debug')(`app:${path.basename(__filename, '.js')}`);
+
+function bookController(Book) {
+  const queryHandler = (reqQuery) => {
+    const query = {};
+    if (reqQuery.title) query.title = reqQuery.title;
+    if (reqQuery.author) query.author = reqQuery.author;
+    if (reqQuery.genre) query.genre = reqQuery.genre;
+    if (reqQuery.read === 'true' || reqQuery.read === 'false') query.read = reqQuery.read;
+    return query;
+  };
+
+  const getByQuery = (req, res) => {
+    const query = queryHandler(req.query);
+    debug(`method: ${req.method}\nurl: ${req.url}\nquery: ${JSON.stringify(query)}}`);
+    Book.find(query)
+      .then((books) => res.json(books))
+      .catch((err) => res.send(err));
+  }
+
+  const getByID = (req, res) => res.json(req.book);
+  
+  const post = (req, res) => {
+    const book = new Book(req.body);
+    debug(`method: ${req.method}\nurl: ${req.url}\nid: ${book.id}`);
+    book.save()
+      .then((book) => res.status(201).json(book));
+  }
+
+  const put = (req, res) => {
+    const { title, author, genre, read } = req.body;
+    const { book } = req;
+    book.title = title;
+    book.author = author;
+    book.genre = genre;
+    book.read = read;
+    book.save()
+      .then((book) => res.json(book))
+      .catch((err) => res.send(err));
+  }
+
+  const patch = (req, res) => {
+    const { book } = req;
+    Object.entries(req.body).forEach((item) => {
+      const [ key, value ] = item; 
+      book[key] = key === '_id' ? book[key] : value;
+    });
+    book.save()
+      .then((book) => res.json(book))
+      .catch((err) => res.send(err));
+  }
+
+  const deleteBook = (req, res) => {
+    const { book } = req;
+    book.remove()
+      .then(() => res.statusCode(204))
+      .catch((err) => res.send(err));
+  }
+
+  // middlewares
+  const checkIDExists = (req, res, next) => {
+    const { bookId } = req.params;
+    debug(`method: ${req.method}\nurl: ${req.url}\nid: ${bookId}`);
+    Book.findById(bookId)
+        .then((book) => {
+          if(book) {
+            req.book = book;
+            return next();
+          }
+          return res.sendStatus(404);
+        })
+        .catch((err) => res.send(err));
+  }
+
+  return {
+    getByQuery,
+    getByID,
+    post,
+    put,
+    patch,
+    deleteBook,
+    checkIDExists
+  }
+}
+
+module.exports = bookController;
--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -4,74 +4,19 @@ const debug = require('debug')(`app:${path.basename(__filename, '.js')}`);
 
 function router(Book) {
   const bookRouter = express.Router();
-
-  const queryHandler = (reqQuery) => {
-    const query = {};
-    if (reqQuery.title) query.title = reqQuery.title;
-    if (reqQuery.author) query.author = reqQuery.author;
-    if (reqQuery.genre) query.genre = reqQuery.genre;
-    if (reqQuery.read === 'true' || reqQuery.read === 'false') query.read = reqQuery.read;
-    return query;
-  };
+  const bookController = require('../controllers/bookController')(Book);
 
   bookRouter.route('/books')
-    .get((req, res) => {
-      const query = queryHandler(req.query);
-      debug(`method: ${req.method}\nurl: ${req.url}\nquery: ${query}`);
-      Book.find(query)
-        .then((books) => res.json(books))
-        .catch((err) => res.send(err));
-    })
-    .post((req, res) => {
-      const book = new Book(req.body);
-      debug(`method: ${req.method}\nurl: ${req.url}\id: ${book.id}`);
-      book.save()
-        .then((book) => res.status(201).json(book));
-    });
+    .get(bookController.getByQuery)
+    .post(bookController.post);
 
-  bookRouter.use('/books/:bookId', (req, res, next) => {
-    const { bookId } = req.params;
-    debug(`method: ${req.method}\nurl: ${req.url}\nid: ${bookId}`);
-    Book.findById(bookId)
-        .then((book) => {
-          if(book) {
-            req.book = book;
-            return next();
-          }
-          return res.sendStatus(404);
-        })
-        .catch((err) => res.send(err));
-  });
-
+  // bookRouter.use('/books/:bookId', );
   bookRouter.route('/books/:bookId')
-    .get((req, res) => res.json(req.book))
-    .put((req, res) => {
-      const { title, author, genre, read } = req.body;
-      const { book } = req;
-      book.title = title;
-      book.author = author;
-      book.genre = genre;
-      book.read = read;
-      book.save()
-        .then((book) => res.json(book))
-        .catch((err) => res.send(err));
-    })
-    .patch((req, res) => {
-      const { book } = req;
-      Object.entries(req.body).forEach((item) => {
-        const [ key, value ] = item; 
-        book[key] = key === '_id' ? book[key] : value;
-      });
-      book.save()
-        .then((book) => res.json(book))
-        .catch((err) => res.send(err));
-    })
-    .delete((req, res) => {
-      const { book } = req;
-      book.remove()
-        .then(() => res.statusCode(204))
-        .catch((err) => res.send(err));
-    })
+    .all(bookController.checkIDExists)
+    .get(bookController.getByID)
+    .put(bookController.put)
+    .patch(bookController.patch)
+    .delete(bookController.deleteBook);
 
   return bookRouter;
 }
